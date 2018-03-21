9016cae97548910a30ac53dd1848a9829a5319be
Francesc Campoy <campoy@golang.org>
2018-03-21 14:45:41 -0700

    first PR

--- goodgopher/goodgopher.go
+++ goodgopher/goodgopher.go
@@ -2,6 +2,7 @@ package goodgopher
 
 import "net/http"
 
+// New returns an http.Handler that provides the entrypoints needed for goodgopher.
 func New(appID int, key, scret []byte, transport http.RoundTripper) (http.Handler, error) {
 	return nil, nil
 }
--- main.go
+++ main.go
@@ -2,15 +2,28 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
+	"net/http/httputil"
+
+	"github.com/kelseyhightower/envconfig"
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
+	var config struct {
+		IP   string `default:"0.0.0.0"`
+		Port string `default:"8080"`
+	}
+	envconfig.MustProcess("", &config)
+
 	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe("0.0.0.0:4321", nil))
+	addr := config.IP + ":" + config.Port
+	logrus.Infof("listening on %s", addr)
+	logrus.Fatal(http.ListenAndServe(addr, nil))
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "hello")
+	b, _ := httputil.DumpRequest(r, true)
+	logrus.Debugf("request: %s", b)
 }
